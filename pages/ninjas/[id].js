f8a6f18236debc2184536d828ce1755cf540afba
gilbertlc <gilbert.lacasandile@gmail.com>
2021-03-28 19:43:09 +0800

    add missing titles

--- pages/ninjas/[id].js
+++ pages/ninjas/[id].js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+
 export const getStaticPaths = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
   const data = await res.json();
@@ -27,12 +29,17 @@ export const getStaticProps = async (context) => {
 
 const Details = ({ninja}) => {
   return (
-    <div>
-      <h1>{ninja.name}</h1>
-      <p>{ninja.email}</p>
-      <p>{ninja.website}</p>
-      <p>{ninja.address.city}</p>
-    </div>
+    <>
+      <Head>
+        <title>Ninja {ninja.name}</title>
+      </Head>
+      <div>
+        <h1>{ninja.name}</h1>
+        <p>{ninja.email}</p>
+        <p>{ninja.website}</p>
+        <p>{ninja.address.city}</p>
+      </div>
+    </>
   );
 };
 
--- pages/ninjas/index.js
+++ pages/ninjas/index.js
@@ -1,5 +1,6 @@
 import styles from "../../styles/Ninjas.module.css";
 import Link from "next/link";
+import Head from "next/head";
 
 export const getStaticProps = async () => {
   const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -12,16 +13,22 @@ export const getStaticProps = async () => {
 
 function ninjas({ninjas}) {
   return (
-    <div>
-      <h1>All Ninjas</h1>
-      {ninjas.map((ninja) => (
-        <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
-          <a className={styles.single}>
-            <h3>{ninja.name}</h3>
-          </a>
-        </Link>
-      ))}
-    </div>
+    <>
+      <Head>
+        <title>Ninja List | Ninjas</title>
+      </Head>
+
+      <div>
+        <h1>All Ninjas ({ninjas.length})</h1>
+        {ninjas.map((ninja) => (
+          <Link href={`/ninjas/${ninja.id}`} key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </Link>
+        ))}
+      </div>
+    </>
   );
 }
 
